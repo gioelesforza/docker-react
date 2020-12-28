c05d4dd28d93aa2b91f5d178834d6e718c1537ff
gioelesforza <gioele.sforza@exprivia.com>
2020-12-28 10:55:38 +0100

    Added message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Come there.
+          Come here.
         </p>
         <a
           className="App-link"
